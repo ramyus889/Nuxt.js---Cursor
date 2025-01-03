f2ec3b0ef1863cab13a8914f14f2bb6f8c957e34
ramyus889 <ramyus889.com>
2025-01-03 10:25:53 +0300

    Обновлен компонент UseHeader для улучшения стилей и адаптивности, изменены отступы. Исправлены анимации в transitionApp.css для более плавного перехода.

--- components/UseHeader.vue
+++ components/UseHeader.vue
@@ -4,7 +4,7 @@
   <div class="fixed top-0 left-0 right-0 z-10">
     <div class="flex place-content-center font-geist-mono">
       <div
-        class="mt-[24px] min-[2300px]:mx-[435px] min-[2000px]:mx-[300px] min-[1600px]:mx-[200px] min-[1280px]:mx-[100px] mx-[20px] rounded-[16px] flex justify-between items-center bg-black px-[7px] pt-[7px] pb-[8px] w-full"
+        class="mt-[24px] min-[2300px]:mx-[435px] min-[2000px]:mx-[300px] min-[1600px]:mx-[200px] min-[1280px]:mx-[100px] mx-[20px] rounded-[16px] flex justify-between items-center bg-black px-[10px] pt-[7px] pb-[8px] w-full"
       >
         <div class="">
           <NuxtLink to="/">
@@ -12,7 +12,7 @@
           </NuxtLink>
         </div>
 
-        <div class="sm:flex hidden gap-[44px]">
+        <div class="sm:flex hidden gap-[44px] me-3">
           <NuxtLink to="/UsePricing" class="uppercase">Pricing</NuxtLink>
           <NuxtLink to="/UseFeatures" class="uppercase">Features</NuxtLink>
         </div>
